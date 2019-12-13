8b72cc95053f4ea4aa1276e9c77b5191fbfa691e
Yisus <carlos.ruiz@inmersys.com>
2019-12-13 10:58:20 -0600

    Themes View

--- src/views/Themes.vue
+++ src/views/Themes.vue
@@ -3,8 +3,10 @@
     <a-Row :gutter="2">
       <a-col :span="21">
         <a-row>
-          <h1 class="title-theme">BIODERMA GAMES</h1>
-          <a-card style="width:100%; min-height:300px;"></a-card>
+          <a-card style="width:100%; min-height:300px;">
+            <h1 class="title-theme">BIODERMA GAMES</h1>
+            <a-divider />
+          </a-card>
         </a-row>
       </a-col>
       <a-col :span="3">
@@ -12,12 +14,19 @@
           shape="circle"
           icon="plus"
           size="large"
-          style="margin-left:70px; margin-top:50px;"
+          style="margin-left:60px; margin-top:50px;"
         />
-      </a-col>
-      <a-col class="title-span-tag">AÑADIR PRODUCTO</a-col>
+        <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AÑADIR IMAGEN
+        <br />
+        <br />
+        <a-switch
+          style="margin-left:60px; margin-top:30px;"
+          defaultChecked
+          @change="checkedBioderma"
+        ></a-switch>
+      </a-col>Activar Bioderma Games
     </a-Row>
-    <a-Row :gutter="3">
+    <a-Row :gutter="2">
       <a-col :span="14" style="margin-top: 50px;">
         <a-row>
           <h1 class="title-theme">SELECCIONA UN TEMA PARA LA APLICACIÓN</h1>
@@ -97,12 +106,17 @@
           </div>
         </a-row>
       </a-col>
-      <a-col :span="6" style="margin-top: 50px;">
+      <a-col :span="7" style="margin-top: 50px;">
         <a-row>
-          <h1 class="title-theme">TIENDA</h1>
-          <a-card style="width:100%; min-height:400px; margin-left:80px;"></a-card>
+          <a-card style="height:500px; margin-left:60px;">
+            <h1 class="title-theme">TIENDA</h1>
+            <a-divider />
+          </a-card>
         </a-row>
       </a-col>
+      <a-col :span="3">
+        <a-switch style="margin-left:80px; margin-top:80px;" defaultChecked @change="Storechecked"></a-switch>
+      </a-col>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Activar Tienda
     </a-Row>
   </div>
 </template>
@@ -115,6 +129,12 @@ export default {
     };
   },
   methods: {
+    Storechecked(value) {
+      console.log(value);
+    },
+    checkedBioderma(value) {
+      console.log(value);
+    },
     changeSessionColor() {
       this.$confirm({
         title: "¿QUE TAL ESTE TEMA PARA TU APLICACIÓN?",
