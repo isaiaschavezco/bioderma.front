2bf8d007b0d0ce35effc009a6fec849b6678f888
Yisus <carlos.ruiz@inmersys.com>
2019-12-16 17:24:40 -0600

    Congiration Services

--- src/views/Themes.vue
+++ src/views/Themes.vue
@@ -15,10 +15,87 @@
           icon="plus"
           size="large"
           style="margin-left:60px; margin-top:50px;"
+          @click="addImageModal=true"
         />
         <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AÑADIR IMAGEN
         <br />
         <br />
+        <a-modal
+          title="ACTIVAR BIODERMA GAMES"
+          v-model="activateBioGames"
+          okText="Si"
+          cancelText="No"
+          @ok="actBioGames"
+        >
+          <p>¿Estás seguro de activar Bioderma Games?</p>
+        </a-modal>
+        <a-modal
+          title="DESACTIVAR BIODERMA GAMES"
+          v-model="desactivateBioGames"
+          okText="Si"
+          cancelText="No"
+          @ok="desBioGames"
+        >
+          <p>¿Estás seguro de desactivar Bioderma Games?</p>
+        </a-modal>
+        <a-modal
+          title="ACTIVAR TIENDA"
+          v-model="activateStore"
+          okText="Si"
+          cancelText="No"
+          @ok="actStore"
+        >
+          <p>¿Estás seguro de activar la Tienda?</p>
+        </a-modal>
+        <a-modal
+          title="DESACTIVAR BIODERMA GAMES"
+          v-model="desactivateStore"
+          okText="Si"
+          cancelText="No"
+          @ok="desStore"
+        >
+          <p>¿Estás seguro de desactivar la Tienda?</p>
+        </a-modal>
+        <a-modal title="Añadir una imagen" v-model="addImageModal" centered>
+          <a-form :form="imageForm">
+            <a-form-item>
+              <div class="dropbox">
+                <a-upload-dragger
+                  v-decorator="[
+                'upload',
+                {
+                  rules: [
+                    {
+                      required: false,
+                      message: 'Favor de cargar un archivo JPG, PNG o JPGE'
+                    }
+                  ]
+                }
+              ]"
+                  name="upload"
+                  action="https://bioderma-api-inmersys.herokuapp.com/upload/2"
+                  accept=".png, .jpg, jpge"
+                  @change="handleChangeFileUpload"
+                  :beforeUpload="beforeUpload"
+                  :fileList="fileList"
+                >
+                  <p class="ant-upload-drag-icon">
+                    <a-icon type="picture" />
+                  </p>
+                  <p class="ant-upload-text">Selecciona o suelta una imagen para tu producto</p>
+                  <p class="ant-upload-hint">Unicamente archivos .png, .jpg o .jpge</p>
+                </a-upload-dragger>
+              </div>
+            </a-form-item>
+          </a-form>
+          <template slot="footer">
+            <a-button
+              type="primary"
+              style="background-color:#009FD1; border-radius: 24px; width: 200px; margin-bottom: 20px;"
+              @click="onSubmitPictureForm"
+            >Aceptar</a-button>
+          </template>
+        </a-modal>
         <a-switch
           style="margin-left:60px; margin-top:30px;"
           defaultChecked
@@ -46,7 +123,7 @@
                 type="primary"
                 style="background: #449C44; border: none;"
                 class="btn-cel"
-                @click="changeSessionColor"
+                @click="changeSessionColor(1)"
               >APLICAR</a-button>
             </a-row>
           </div>
@@ -64,7 +141,7 @@
                 type="primary"
                 style="background: #F99F28; border: none;"
                 class="btn-cel"
-                @click="changeSessionColor"
+                @click="changeSessionColor(2)"
               >APLICAR</a-button>
             </a-row>
           </div>
@@ -82,7 +159,7 @@
                 type="primary"
                 style="background: #AB6870; border: none;"
                 class="btn-cel"
-                @click="changeSessionColor"
+                @click="changeSessionColor(3)"
               >APLICAR</a-button>
             </a-row>
           </div>
@@ -100,7 +177,7 @@
                 type="primary"
                 style="background: #0077AA; border: none;"
                 class="btn-cel"
-                @click="changeSessionColor"
+                @click="changeSessionColor(4)"
               >APLICAR</a-button>
             </a-row>
           </div>
@@ -125,22 +202,219 @@ const data = [];
 export default {
   data() {
     return {
+      activateBioGames: false,
+      desactivateBioGames: false,
+      activateStore: false,
+      desactivateStore: false,
+      addImageModal: false,
+      image: "",
+      imageForm: this.$form.createForm(this),
+      isClubBiodermaActive: true,
+      isBiodermaGameActive: true,
+      fileList: [],
+      themes: 1,
+      general: [
+        {
+          id: 0,
+          isClubBiodermaActive: true,
+          themes: 1,
+          isBiodermaGameActive: true,
+          biodermaGameImage: ""
+        }
+      ],
       data
     };
   },
+  mounted() {
+    this.getGeneral();
+  },
   methods: {
+    async getGeneral() {
+      const responseGeneral = await this.$axios.get("configutarion/general");
+      console.log(responseGeneral.data.general);
+      this.general = responseGeneral.data.general;
+    },
     Storechecked(value) {
-      console.log(value);
+      //console.log(value);
+      if (value) {
+        this.activateStore = true;
+      } else {
+        this.desactivateStore = true;
+      }
+    },
+    failActStore() {
+      this.$notification["error"]({
+        message: "ERROR EN TIENDA",
+        description:
+          "Se ha proudcido un error intentando activar la tienda, favor de intentarlo más tarde"
+      });
+    },
+    failDesStore() {
+      this.$notification["error"]({
+        message: "ERROR EN TIENDA",
+        description:
+          "Se ha proudcido un error intentando desactivar la tienda, favor de intentarlo más tarde"
+      });
+    },
+    failActGames() {
+      this.$notification["error"]({
+        message: "ERROR EN BIODERMA GAMES",
+        description:
+          "Se ha proudcido un error intentando activar Bioderma Games, favor de intentarlo más tarde"
+      });
+    },
+    failDesGames() {
+      this.$notification["error"]({
+        message: "ERROR EN TIENDA",
+        description:
+          "Se ha proudcido un error intentando desactivar Bioderma Games, favor de intentarlo más tarde"
+      });
+    },
+    onSubmitPictureForm() {
+      this.imageForm.validateFields((err, values) => {
+        if (!err) {
+          image: values.upload.fileList[0].response;
+          //console.log(this.fileList[0].response);
+        } else {
+          console.log("Huevos");
+        }
+      });
+    },
+    successAddingImage() {
+      //Agregando una imagen
+    },
+    failAddingImage() {
+      this.$notification["error"]({
+        message: "ERROR AL AÑADIR UNA IMAGEN",
+        description:
+          "Se ha proudcido un error añdiendo una imagen, favor de intentarlo más tarde."
+      });
+    },
+    handleChangeFileUpload(info) {
+      let fileList = [...info.fileList];
+      fileList = fileList.slice(-1);
+      this.fileList = fileList;
+    },
+    beforeUpload(file) {
+      let status = true;
+      this.imageForm.validateFields((err, values) => {
+        if (err) {
+          if (err.menu || err.submenu || err.title) {
+            status = false;
+          }
+        }
+      });
+      return status;
+    },
+    async actStore() {
+      try {
+        const responseStore = await this.$axios.post("configutarion/club", {
+          isClubBiodermaActive: true
+        });
+        if (responseStore.data.status == 0) {
+          this.getGeneral();
+        } else {
+          if (value) {
+            this.failActStore;
+          } else {
+            this.failDesStore;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      this.activateStore = false;
+    },
+    async desStore() {
+      try {
+        const responseStore = await this.$axios.post("configutarion/club", {
+          isClubBiodermaActive: false
+        });
+        if (responseStore.data.status == 0) {
+          this.getGeneral();
+        } else {
+          if (value) {
+            this.failActStore;
+          } else {
+            this.failDesStore;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      this.desactivateStore = false;
+    },
+    async actBioGames() {
+      //console.log("activar");
+      try {
+        const responseBGames = await this.$axios.post(
+          "configutarion/bioderma",
+          {
+            isClubBiodermaActive: true
+          }
+        );
+        if (responseBGames.data.status == 0) {
+          this.getGeneral();
+        } else {
+          if (value) {
+            this.failActGames;
+          } else {
+            this.failDesGames;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      this.activateBioGames = false;
+    },
+    async desBioGames() {
+      //console.log("desactivar");
+      try {
+        const responseBGames = await this.$axios.post(
+          "configutarion/bioderma",
+          {
+            isClubBiodermaActive: false
+          }
+        );
+        if (responseBGames.data.status == 0) {
+          this.getGeneral();
+        } else {
+          if (value) {
+            this.failActGames;
+          } else {
+            this.failDesGames;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      this.desactivateBioGames = false;
     },
     checkedBioderma(value) {
-      console.log(value);
+      if (value) {
+        this.activateBioGames = true;
+      } else {
+        this.desactivateBioGames = true;
+      }
     },
-    changeSessionColor() {
+    async getThemes(num) {
+      try {
+        const responseThemes = await this.$axios.post("configutarion/theme", {
+          themes: this.num
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      this.getGeneral();
+    },
+    changeSessionColor(num) {
+      console.log(num);
+      const component = this;
       this.$confirm({
         title: "¿QUE TAL ESTE TEMA PARA TU APLICACIÓN?",
         content: "",
         onOk() {
-          alert("Theme Actived");
+          component.getThemes(num);
         },
         onCancel() {}
       });
